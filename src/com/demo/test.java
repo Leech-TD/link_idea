e360967f58b0d2bd867c5814f6cfab0c58e7a719
Ricci <15277864831@163.com>
2022-03-13 18:03:11 +0800

    version3

--- src/com/demo/test.java
+++ src/com/demo/test.java
@@ -4,6 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("version 1");
         System.out.println("version 2");
+        System.out.println("version 3");
 
     }
 }
